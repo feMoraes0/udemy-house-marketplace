586df4f4a12b4a9c9b3d12a0fcb4c28791eb4c07
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2023-05-08 08:53:45 -0300

    feat: creates pages files and adds as routes to App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import ForgotPassword from "./pages/ForgotPassword"
+import Offers from "./pages/Offers"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold">Hello world</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
   )
 }
 
