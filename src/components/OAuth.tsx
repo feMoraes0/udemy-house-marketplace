6794a383dc26ca12a7d43ea38952a55a21776323
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2023-11-06 11:31:15 -0300

    feat: updates oauth button with google login

---
+++ src/components/OAuth.tsx
@@ -0,0 +1,43 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { FcGoogle } from 'react-icons/fc';
+import { toast } from 'react-toastify';
+import { db } from '../firebase';
+import { useNavigate } from 'react-router';
+
+const OAuth = () => {
+  const navigate = useNavigate();
+
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const { user } = await signInWithPopup(auth, provider);
+      const docRef = doc(db, "users", user.uid);
+      const docSnapshot = await getDoc(docRef);
+      if(!docSnapshot.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+    } catch (error) {
+      toast.error("Not authorized with Google");
+    }
+  };
+
+  return (
+    <button
+      onClick={onGoogleClick}
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-200 ease-in-out rounded"
+      type="button"
+    >
+      <FcGoogle className="text-2xl bg-white rounded-full mr-2" />
+      Continue with Google
+    </button>
+  )
+}
+
+export default OAuth;
