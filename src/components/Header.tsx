638a96e633ac6f01a5eaa6e8dadfc82c2d0c0a1e
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2023-11-13 15:10:25 -0300

    feat: updates header to show profile when authenticated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
   const location = useLocation();
   const navitate = useNavigate();
+  const auth = getAuth();
+  const [pageState, setPageState] = useState("Sign in");
 
   const pathMatchRoute = (route: string) => !!(route === location.pathname);
   const getListItemClassByRoute = (route: string): string => {
@@ -10,6 +14,13 @@ const Header = () => {
       ? "text-black border-b-red-500"
       : "text-gray-400 border-b-transparent";
   }
+
+  useEffect(() => {
+    onAuthStateChanged(
+      auth,
+      (user) => setPageState(user ? "Profile" : "Sign in")
+    );
+  }, [auth]);
 
   return (
     <div className="bg-white border-b shadow-sm sticky top-0 z-50">
@@ -45,11 +56,11 @@ const Header = () => {
             <li
               className={`
                 cursor-pointer py-3 text-sm font-semibold border-b-[3px]
-                ${getListItemClassByRoute("/sign-in")}
+                ${getListItemClassByRoute(pageState === "Sign in" ? "/sign-in" : "/profile")}
               `}
-              onClick={() => navitate('/sign-in')}
+              onClick={() => navitate('/profile')}
             >
-              Sign in
+              { pageState }
             </li>
           </ul>
         </div>
